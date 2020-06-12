5f844b9703ff34d42c5d07bfc2dd51155e5766bc
David Tarrant <davetaz@theodi.org>
2020-06-12 15:04:15 +0100

    Adding first itteration of theme for framework v5

--- js/theme.js
+++ js/theme.js
@@ -1,13 +1,41 @@
-var theme="odi-generic";
-define(function(require) {
-	
-	var Adapt = require('coreJS/adapt');
-	var Backbone = require('backbone');
-	var ThemeBlock = require('theme/adapt-theme-odi-generic/js/theme-block');
-	var msjquery = require('theme/adapt-theme-odi-generic/js/jquery.dd.js');
-	var emailPresent = false;
+//var theme="odi-generic";
+define([
+  "core/js/adapt",
+  "./themePageView",
+  "./themeArticleView",
+  "./themeBlockView",
+  "./themeView"
+], function(Adapt, ThemePageView, ThemeArticleView, ThemeBlockView, ThemeView) {
 
-	// Block View
+	function onDataReady() {
+    	$("html").addClass(Adapt.course.get("_courseStyle"));
+  	}
+
+  	function onPostRender(view) {
+    	var model = view.model;
+    	var theme = model.get("_vanilla");
+
+    	if (!theme) return;
+
+		switch (model.get("_type")) {
+			case "page":
+				new ThemePageView({ model: new Backbone.Model(theme), el: view.$el });
+				break;
+      		case "article":
+        		new ThemeArticleView({ model: new Backbone.Model(theme), el: view.$el });
+        		break;
+      		case "block":
+        		new ThemeBlockView({ model: new Backbone.Model(theme), el: view.$el });
+        		break;
+      		default:
+        		new ThemeView({ model: new Backbone.Model(theme), el: view.$el });
+    	}
+  	}
+
+  	Adapt.on({
+    	"app:dataReady": onDataReady,
+    	"pageView:postRender articleView:postRender blockView:postRender": onPostRender
+  	});
 	// ==========
 
 	Adapt.on('blockView:postRender', function(view) {
---
+++ js/themeBlockView.js
@@ -0,0 +1,68 @@
+define([
+  "./themeView",
+  "core/js/adapt"
+], function(ThemeView, Adapt) {
+
+  var ThemeBlockView = ThemeView.extend({
+
+		initialize: function() {
+			this.setStyles();
+			this.listenTo(Adapt, 'device:resize', this.setStyles);
+			this.listenTo(Adapt, 'remove', this.remove);
+		},
+
+		setStyles: function() {
+			this.setBackground();
+			this.setMinHeight();
+			this.setDividerBlock();
+		},
+
+		setBackground: function() {
+			var backgroundColor = this.model.get('_themeBlockConfig')._backgroundColor;
+			
+			if (backgroundColor) {
+				this.$el.addClass(backgroundColor);
+			}
+		},
+
+		setMinHeight: function() {
+			var minHeight = 0;
+			var minHeights = this.model.get('_themeBlockConfig')._minimumHeights;
+
+			if (minHeights) {
+
+				if(Adapt.device.screenSize == 'large') {
+					minHeight = minHeights._large;
+				} else if (Adapt.device.screenSize == 'medium') {
+					minHeight = minHeights._medium;
+				} else {
+					minHeight = minHeights._small;
+				}
+			}
+
+			this.$el.css({
+				minHeight: minHeight + "px"
+			});
+		},
+
+		setDividerBlock: function() {
+			var dividerBlock = this.model.get('_themeBlockConfig')._isDividerBlock;
+
+			if (dividerBlock) {
+				this.$el.addClass('divider-block');
+			}
+		},
+
+    	className: function() {
+      		return this.model.get("_isDividerBlock") ? "is-divider-block" : "";
+    	},
+
+    	setCustomStyles: function() {},
+
+    	onRemove: function() {}
+
+  });
+
+  return ThemeBlockView;
+
+});
